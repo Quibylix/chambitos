c65432ef7644160e3e4e205d1412bcd679c800d0
Quibylix <barraza.fredimanuel@gmail.com>
2024-04-05 14:50:18 -0600

    feat: add Features component to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Features from "@/features/ui/components/features.component";
 import Hero from "@/features/ui/components/hero.component";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <Features />
     </main>
   );
 }
