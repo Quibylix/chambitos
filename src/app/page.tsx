f4d5aef248dd3d5b2370738eb16ec8d962aafcd2
Quibylix <barraza.fredimanuel@gmail.com>
2024-04-07 19:11:10 -0600

    feat: add Footer component to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Features from "@/features/ui/components/features.component";
+import Footer from "@/features/ui/components/footer.component";
 import Hero from "@/features/ui/components/hero.component";
 import Reviews from "@/features/ui/components/reviews.component";
 
 export default function HomePage() {
   return (
-    <main>
-      <Hero />
-      <Features />
-      <Reviews />
-    </main>
+    <>
+      <main>
+        <Hero />
+        <Features />
+        <Reviews />
+      </main>
+      <Footer />
+    </>
   );
 }
