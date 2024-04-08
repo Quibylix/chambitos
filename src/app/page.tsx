8e22abb375553296d63b8ea363db13435e514138
Quibylix <barraza.fredimanuel@gmail.com>
2024-04-07 18:47:57 -0600

    feat: add Reviews component to HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Features from "@/features/ui/components/features.component";
 import Hero from "@/features/ui/components/hero.component";
+import Reviews from "@/features/ui/components/reviews.component";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <Features />
+      <Reviews />
     </main>
   );
 }
