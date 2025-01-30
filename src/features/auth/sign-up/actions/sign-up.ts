b39a44e8755d7cfb7aa302ae489bf61546b23a3c
Quibylix <barraza.fredimanuel@gmail.com>
2025-01-30 13:46:24 -0600

    feat: add sign up action

---
+++ src/features/auth/sign-up/actions/sign-up.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { createClient } from "@/features/db/utils/server";
+import { validatePassword } from "../../helpers/validate-password";
+import { validateEmail } from "../../helpers/validate-email";
+
+export type SignUpData = {
+  email: string;
+  password: string;
+};
+
+export async function signUp({ email, password }: SignUpData) {
+  const db = await createClient();
+
+  const errors = [validateEmail(email), validatePassword(password)].filter(
+    Boolean,
+  );
+
+  if (errors.length) {
+    return {
+      success: false,
+      error: errors.join(", "),
+    };
+  }
+
+  const { error } = await db.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    console.log(error);
+
+    return {
+      success: false,
+      error: "An error occurred while signing up. Please try again later.",
+    };
+  }
+
+  return {
+    success: false,
+    error: "An error occurred while signing up. Please try again later.",
+  };
+
+  return { success: true };
+}
