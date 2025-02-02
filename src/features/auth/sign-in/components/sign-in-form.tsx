6dc2121ba0f3c3b8867e10c5be24052d6a2f42bb
Quibylix <barraza.fredimanuel@gmail.com>
2025-02-02 09:29:21 -0600

    refactor: remove unused AuthenticationTitle function

--- src/features/auth/sign-in/components/sign-in-form.tsx
+++ src/features/auth/sign-in/components/sign-in-form.tsx
@@ -1,52 +1,9 @@
 "use client";
 
-import {
-  Anchor,
-  Checkbox,
-  Container,
-  Group,
-  Paper,
-  Title,
-} from "@mantine/core";
-//  import classes from "./AuthenticationTitle.module.css";
-
-export function AuthenticationTitle() {
-  return (
-    <Container size={420} my={40}>
-      <Title ta="center">Welcome back!</Title>
-      <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Anchor size="sm" component="button">
-          Create account
-        </Anchor>
-      </Text>
-
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="you@mantine.dev" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Group justify="space-between" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
-          </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
-      </Paper>
-    </Container>
-  );
-}
-
 import { useForm } from "@mantine/form";
 import { validateEmail } from "../../helpers/validate-email";
 import { validatePassword } from "../../helpers/validate-password";
-import { Button, PasswordInput, Text, TextInput } from "@mantine/core";
+import { Paper, Button, PasswordInput, Text, TextInput } from "@mantine/core";
 import { useRouter } from "next/navigation";
 import { signIn } from "../actions/sign-in";
 
