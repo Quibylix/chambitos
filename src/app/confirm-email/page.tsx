249101e4183179021cd246cd3c11f2e36f469627
Quibylix <barraza.fredimanuel@gmail.com>
2025-02-01 18:38:29 -0600

    feat: stylize the email confirmation page

--- src/app/confirm-email/page.tsx
+++ src/app/confirm-email/page.tsx
@@ -1,8 +1,43 @@
+import { Container, Flex, Image, Stack, Text, Title } from "@mantine/core";
+import NextImage from "next/image";
+import mailSent from "@/assets/mail-sent.png";
+
 export default function ConfirmEmailPage() {
   return (
-    <div>
-      <h1>Confirm Email</h1>
-      <p>Check your email for a confirmation link.</p>
-    </div>
+    <Container size="md" p="md">
+      <Flex justify="center" wrap="wrap">
+        <Stack
+          miw={250}
+          maw={300}
+          gap="lg"
+          align="center"
+          justify="center"
+          ta="center"
+        >
+          <Title>
+            Confirm{" "}
+            <Text inherit span c="blue">
+              Your Email
+            </Text>{" "}
+            Address
+          </Title>
+          <Text size="lg">Thank you for signing up!</Text>
+          <Text size="lg" c="dimmed">
+            We have sent you a confirmation email. Please check your inbox and
+            follow the instructions to complete the registration.
+          </Text>
+        </Stack>
+        <Image
+          component={NextImage}
+          src={mailSent.src}
+          alt="Email"
+          maw="100%"
+          h="auto"
+          w={400}
+          width={mailSent.width}
+          height={mailSent.height}
+        />
+      </Flex>
+    </Container>
   );
 }
